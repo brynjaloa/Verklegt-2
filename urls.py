0fb7bb1047e15a7b45e08cc81396eb670d8581c5
juliavidis <juliamist1@gmail.com>
2026-05-06 14:40:05 +0000

    css og virkni fyrir login og signup

--- urls.py
+++ urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
+from . import views
 
-
-urlpatterns = [
-    path('', views.home, name='home'),
+    path('login/', views.login_view, name='login'),
+    path('signup/', views.signup_view, name='signup'),
 ]
