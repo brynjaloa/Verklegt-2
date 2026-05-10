c68f7ee2b638fb1b1f229959f952f865247a4af8
juliavidis <juliamist1@gmail.com>
2026-05-10 18:10:22 +0000

    -

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 from artworks.views import home_view
 
 
@@ -35,4 +36,3 @@ if settings.DEBUG:
         settings.MEDIA_URL,
         document_root=settings.MEDIA_ROOT
     )
-
